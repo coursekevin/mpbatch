fb87477902ec52173202a2cee50ef1a12f5635b9
Kevin Course <coursekevin@Kevins-MacBook-Pro.local>
2019-03-17 15:55:42 -0400

    Updates to setup, removed unecessary files

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='mpbatch',
-	version='0.0',
-	packages=['mpbatch'],
-	description="Multiprocessing of batch processes",
-	author="Kevin Course",
-	author_email="kevinlcourse@gmail.com",
-	)
+with open("README.md","r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name = "mpbatch",
+	version = "0.1",
+	author ="Kevin Course",
+	author_email = "kevin.course@mail.utoronto.ca",
+	description="Easy multi-core processing of batch-type processes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/coursekevin/avlpy",
+    install_requires=['multiprocessing'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
